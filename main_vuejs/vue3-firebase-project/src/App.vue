653eec5a9a21c980ac9b303820fbf305218a5424
Alex <60273062+zwaky@users.noreply.github.com>
2023-03-30 15:08:09 -0400

    Basic Front end and Back End

--- main_vuejs/vue3-firebase-project/src/App.vue
+++ main_vuejs/vue3-firebase-project/src/App.vue
@@ -6,24 +6,6 @@
           <h1 class="title">Indeed 2.0</h1>
         </a>
       </div>
-
-      <div class="navbar-menu">
-        <div class="navbar-start">
-          <router-link to="/" class="navbar-item">Home</router-link>
-          <router-link to="/about" class="navbar-item">About</router-link>
-          <router-link v-if="isLoggedOut" to="/register" class="navbar-item">Register</router-link>
-          <router-link v-if="!isLoggedOut" to="/addjob" class="navbar-item">Edit Jobs</router-link>
-        </div>
-
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <div class="buttons">
-              <button v-on:click="handleSignOut" v-if="isLoggedOut" class="button is-light">Sign in</button>
-              <button v-on:click="handleSignOut" v-if="!isLoggedOut" class="button is-light">Sign out</button>
-            </div>
-          </div>
-        </div>
-      </div>
     </nav>
   </header>
 
@@ -40,27 +22,11 @@ import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 
 export default {
   data() {
-    return {
-      isLoggedOut: false,
-    };
+
   },
   mounted() {
-    const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
-      if (!user) {
-        this.isLoggedOut = true;
-      } else {
-        this.isLoggedOut = false;
-      }
-    });
   },
   methods: {
-    handleSignOut() {
-      const auth = getAuth();
-      signOut(auth).then(() => {
-        this.$router.push("/login");
-      });
-    },
   },
 };
 </script>
--- main_vuejs/vue3-firebase-project/src/router/index.js
+++ main_vuejs/vue3-firebase-project/src/router/index.js
@@ -16,39 +16,8 @@ const router = createRouter({
       path: '/about',
       name: 'About',
       component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: () => import('../views/Login.vue')
-    },
-    {
-      path: '/register',
-      name: 'Register',
-      component: () => import('../views/Register.vue')
-    },
-    {
-      path: '/addjob',
-      name: 'AddJob',
-      component: () => import('../views/AddJob.vue')
     }
   ]
 })
-
-//Code to restrict which pages can be accessed by which users
-// router.beforeEach((to, from, next) =>{
-// if(to.matched.some((record) => record.meta.requiresAuth)){
-
-//   if(getAuth().currentUser){
-//     next();
-//   } else{
-//     alert("you dont have access!");
-//     next("/about");
-//   }
-// }else{
-//   next();
-// }
-
-// });
 
 export default router
