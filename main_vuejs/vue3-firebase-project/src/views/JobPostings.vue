f593a3104b39d98f7a38c22c925e0114e5b6b35b
Alex <60273062+zwaky@users.noreply.github.com>
2023-03-31 08:19:24 -0400

    Fixed view job postings

--- main_vuejs/vue3-firebase-project/src/views/JobPostings.vue
+++ main_vuejs/vue3-firebase-project/src/views/JobPostings.vue
@@ -1,93 +1,96 @@
-<template>
-  <header>
-    <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <a class="navbar-item" href="/">
-          <h1 class="title">Indeed 2.0</h1>
-        </a>
-      </div>
-
-      <div class="navbar-menu">
-        <div class="navbar-start">
-          <router-link to="/" class="navbar-item">Home</router-link>
-          <router-link to="/about" class="navbar-item">About</router-link>
-          <router-link v-if="isLoggedOut" to="/register" class="navbar-item">Register</router-link>
-          <router-link v-if="isEmployer" to="/addjob" class="navbar-item">Post Jobs</router-link>
-          <router-link v-if="isCandidate" to="/jobs" class="navbar-item">Browse Jobs</router-link>
-          <router-link v-if="isCandidate" to="/applications" class="navbar-item">View Applications</router-link>
-          <router-link v-if="isEmployer" to="/editEmployer" class="navbar-item">Edit Employer</router-link>
-        </div>
-
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <div class="buttons">
-              <button v-on:click="handleSignOut" v-if="isLoggedOut" class="button is-light">Sign in</button>
-              <button v-on:click="handleSignOut" v-if="!isLoggedOut" class="button is-light">Sign out</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-  </header>
-
-  <section class="section">
-    <div class="container">
-      <router-view />
-      
+<template>  
+  <div v-if="jobPostings.length > 0">
+      <h2 class="title is-4">Job Postings</h2>      
+        <div v-for="posting in jobPostings" :key="posting.id" class="card mb-1">          
+            <div class="card-content">
+              <div class="content">   
+                <div class="columns is-mobile is-vcentered">    
+                  <div class="column"> 
+                    <p class="card-header-title">{{ posting.title }}</p>            
+                    {{ posting.description }}
+                  </div>
+                    <div class="column is-5 has-text-right">
+                      <button class="button is-success" v-on:click="ApplytoJob(posting.id)">
+                        Apply
+                      </button>                    
+                  </div> 
+                </div>  
+              </div>
+            </div>          
+        </div>      
     </div>
-  </section>
 </template>
 
+
 <script>
-import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
-import { db } from "@/main"
-import { getCountFromServer, query,where,documentId , collection, doc, getDoc} from 'firebase/firestore'
+import { ref } from 'vue'
+import { db } from '@/main'
+import { collection, addDoc, getDocs, query, where, deleteDoc, doc, getDoc } from 'firebase/firestore'
+import { getAuth } from "firebase/auth";
 
 export default {
+  
+  name: 'AddJob',
   data() {
-    return {
-      isLoggedOut: false,
-      isCandidate: false,
-      isEmployer: false
-    };
-  },
-  mounted() {
-    const auth = getAuth();
-    onAuthStateChanged(auth, async (user) => {
-      if (!user) {
-        this.isLoggedOut = true;
-      } else {
-        this.isLoggedOut = false;
-        if( await(await getCountFromServer(query(collection(db, "employer_profiles"),where(documentId(),'==',user.email)))).data().count==1){
-          this.isEmployer = true;
-        }
-        else{
-          this.isEmployer = false;
-        }
-        if( await(await getCountFromServer(query(collection(db, "candidate_profiles"),where(documentId(),'==',user.email)))).data().count==1){
-          this.isCandidate = true;
-        }
-        else{
-          this.isCandidate = false;
-        }
-        
-      }
-    });
 
+    return {
+      title: '',
+      description: '',
+      jobPostings: [],
+      auth: null
+    }
   },
-  methods: {
-    handleSignOut() {
-      const auth = getAuth();
-      signOut(auth).then(() => {
-        this.isCandidate = false;
-        this.isEmployer = false;
-        this.$router.push("/login");
-      });
+  created() {
+
+    // Get the user object from the promise returned by getAuth()
+    this.auth = getAuth();
+    this.getJobPostings();
+  },
+
+  methods: {  
+
+    async getJobPostings() {
+      const postings = []
+      const q = query(collection(db, 'job_postings')
+     // , where('author', '==', this.auth.currentUser.email)
+      )
+      const querySnapshot = await getDocs(q)
+      querySnapshot.forEach((doc) => {
+        postings.push({ id: doc.id, ...doc.data() })
+      })
+      this.jobPostings = postings
     },
-  },
-};
+
+    async ApplytoJob(postingId) {
+  const currentUser = this.auth.currentUser;
+  const postingRef = doc(db, 'job_postings', postingId);
+  const postingSnapshot = await getDoc(postingRef);
+  const postingData = postingSnapshot.data();
+  
+  // Save application data in database
+  await addDoc(collection(db, 'applications'), {
+    Candidate: currentUser.email,
+    JobPostingId: postingId,
+    Employer: postingData.author,
+    DateApplied: new Date()
+  });
+  
+  // Notify user that application was submitted successfully
+  alert('Application submitted successfully!');
+}
+  }
+}
 </script>
 
-<style>
- @import 'bulma\css\bulma.min.css'; 
+<style scoped>
+.job-posting {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.delete-button {
+  margin-left: 10px;
+}
 </style>
